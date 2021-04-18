10429b67260627d61ee8111652d7acddff95bb09
John Piedrahita <joalpied@bancolombia.com.co>
2021-04-18 14:32:28 -0500

    feat: configuracion entorno de desarrollo

---
+++ src/application/config/environment.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv"
+import fs from "fs"
+
+if (fs.existsSync(".env")) {
+    dotenv.config({ path: ".env" })
+} else {
+    dotenv.config({ path: ".env.example" })
+}
+
+export const ENVIRONMENT = process.env.NODE_ENV;
+const PROD = ENVIRONMENT === "production"
+
+export const SESSION_SECRET = process.env.JWT_SECRET
+export const PORT = process.env.PORT
+export const MONGODB_URI = PROD
+    ? process.env.MONGO_PRODUCTION
+    : process.env.MONGO_PRODUCTION
+
+if (!SESSION_SECRET) process.exit(1)
+
+if (!MONGODB_URI) {
+    if (PROD) {
+        console.log("No mongo connection string. Set MONGODB_URI environment variable.");
+    } else {
+        console.log("No mongo connection string. Set MONGODB_URI_LOCAL environment variable.");
+    }
+    process.exit(1);
+}
+
